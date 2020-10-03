f25bd242432653eed6fdc0f978704fa372b77b7b
amalpradeep-mv <amalpradeepmv@gmail.com>
2020-10-03 12:12:11 +0530

    booking module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddBookingComponent } from './add-booking/add-booking.component';
+import { AllBookingComponent } from './all-booking/all-booking.component';
+import { GetBookingComponent } from './get-booking/get-booking.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'add-booking', component:AddBookingComponent},
+  { path:'getbooking', component:GetBookingComponent},
+  { path: 'allbooking', component:AllBookingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBookingComponent } from './add-booking/add-booking.component';
+import { GetBookingComponent } from './get-booking/get-booking.component';
+import { AllBookingComponent } from './all-booking/all-booking.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BookingService } from './service/service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookingComponent,
+    GetBookingComponent,
+    AllBookingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient,BookingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
